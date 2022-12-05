14505a178b98369a0fa680888e489e14d9010548
amanagarwal97g <amanagarwal.97g@gmail.com>
2022-12-05 18:49:37 +0530

    logofix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import {Link} from "react-router-dom";
-import logo from "../logo.svg";
 import styled from "styled-components";
 import {ButtonContainer} from "./Button";
 import {ProductConsumer} from "../context";
@@ -12,9 +11,6 @@ class Navbar extends Component {
                     const {status} = value
                return( 
            <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
-                <Link to='/'>
-                    <img src={logo} alt="store" className="navbar-brand"></img>
-                </Link>
                 <ul className="navbar-nav align-items-center">
                 <li className="nav-item ml-5">
                     <Link to="/" className="nav-link">
